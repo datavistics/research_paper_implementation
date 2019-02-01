c47d379d0540b02e213fa48e58497d9a099c26cc
datavistics <datavistics@gmail.com>
2019-02-01 17:22:49 +0400

    Updates for automatic cifar_10 download and extraction

--- src/data_processing/cifar_10_helper.py
+++ src/data_processing/cifar_10_helper.py
@@ -2,30 +2,57 @@
 # The CIFAR-10 dataset:
 # https://www.cs.toronto.edu/~kriz/cifar.html
 
+from src.data_processing.utilities import download_and_unzip
 from global_fun import *
 import pickle
 import numpy as np
 import scipy.misc
+import shutil
+
+batch_1_file = cifar_10_dir / 'data_batch_1'
+batch_2_file = cifar_10_dir / 'data_batch_2'
+batch_3_file = cifar_10_dir / 'data_batch_3'
+batch_4_file = cifar_10_dir / 'data_batch_4'
+batch_5_file = cifar_10_dir / 'data_batch_5'
+test_batch_file = cifar_10_dir / 'test_batch'
+
+module_logger = module_logging(__file__, False)
+
+
+@logspeed(module_logger)
+def get_cifar_10(force_download=False):
+    if force_download:
+        module_logger.info("Forcing Download")
+        if cifar_10_dir.exists():
+            shutil.rmtree(cifar_10_dir)
+
+    tar_url = 'https://www.cs.toronto.edu/~kriz/cifar-10-python.tar.gz'
+    outfile_path = data_dir
+
+    if batch_1_file.exists():
+        module_logger.info("Data Already Exists...")
+    else:
+        module_logger.info("Getting Cifar 10 Data...")
+        download_and_unzip(tar_url, outfile_path)
 
 
 def __unpickle(file):
     with open(file, 'rb') as fo:
-        dict = pickle.load(fo, encoding='bytes')
-    return dict
+        unpickled_obj = pickle.load(fo, encoding='bytes')
+    return unpickled_obj
 
 
 def read_cifar_10(image_width, image_height):
-
-    batch_1 = __unpickle(data_dir/'cifar_10'/'data_batch_1.pkl')
-    batch_2 = __unpickle(data_dir/'cifar_10'/'data_batch_1.pkl')
-    batch_3 = __unpickle(data_dir/'cifar_10'/'data_batch_1.pkl')
-    batch_4 = __unpickle(data_dir/'cifar_10'/'data_batch_1.pkl')
-    batch_5 = __unpickle(data_dir/'cifar_10'/'data_batch_1.pkl')
-    test_batch = __unpickle(data_dir/'cifar_10'/'test_batch.pkl')
+    batch_1 = __unpickle(batch_1_file)
+    batch_2 = __unpickle(batch_2_file)
+    batch_3 = __unpickle(batch_3_file)
+    batch_4 = __unpickle(batch_4_file)
+    batch_5 = __unpickle(batch_5_file)
+    test_batch = __unpickle(test_batch_file)
 
     classes = ['airplane', 'automobile', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
 
-    total_train_samples = len(batch_1[b'labels']) + len(batch_2[b'labels']) + len(batch_3[b'labels'])\
+    total_train_samples = len(batch_1[b'labels']) + len(batch_2[b'labels']) + len(batch_3[b'labels']) \
                           + len(batch_4[b'labels']) + len(batch_5[b'labels'])
 
     X_train = np.zeros(shape=[total_train_samples, image_width, image_height, 3], dtype=np.uint8)
@@ -65,3 +92,7 @@ def read_cifar_10(image_width, image_height):
         Y_test[i] = Y
 
     return X_train, Y_train, X_test, Y_test
+
+
+if __name__ == '__main__':
+    get_cifar_10()
---
+++ src/data_processing/utilities.py
@@ -0,0 +1,19 @@
+from global_fun import *
+import urllib.request
+import tarfile
+
+module_logger = module_logging(__file__, False)
+
+
+def download_and_unzip(tar_url, outfile_path):
+    module_logger.info(f'Downloading from: {tar_url}')
+    download_location = outfile_path/Path(tar_url).name
+    urllib.request.urlretrieve(tar_url, outfile_path/Path(tar_url).name)
+    module_logger.info(f'Finished Download')
+
+    with tarfile.open(download_location, 'r:*') as f:
+        for file in f.getnames():
+            module_logger.info(f'Extracting file: {file}')
+        f.extractall(path=outfile_path)
+
+
