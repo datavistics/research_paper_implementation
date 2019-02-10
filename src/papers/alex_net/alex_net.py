d94c01910cdf9dbed7288e23e2fbc0b92917b823
datavistics <datavistics@gmail.com>
2019-02-10 19:55:16 +0400

    Adding training functionality

--- src/papers/alex_net/alex_net.py
+++ src/papers/alex_net/alex_net.py
@@ -1,33 +1,41 @@
-import torch
 import numpy as np
 
-import torch.nn.Functional as F
+from torch import manual_seed
+import torch.optim as optim
+import torch.nn as nn
+import torch.nn.functional as F
 
 
-class AlexNet(torch.nn.module):
+class AlexNet(nn.module):
+    """
 
+    :param seed: Random Seed for torch and numpy
+    :type seed: int
+    """
     def __init__(self, seed: int=42):
         super(AlexNet, self).__init()
 
         np.random.seed(seed)
-        torch.manual_seed(seed)
+        manual_seed(seed)
 
-        self.conv_1 = torch.nn.Conv2d(11, 11, kernel_size=96, stride=4, padding=0)
-        self.pool_1 = torch.nn.MaxPool2d(kernel_size=3, stride=2)
+        self.conv_1 = nn.Conv2d(11, 11, kernel_size=96, stride=4, padding=0)
+        self.pool_1 = nn.MaxPool2d(kernel_size=3, stride=2)
 
-        self.conv_2 = torch.nn.Conv2d(5, 5, kernel_size=256, stride=1, padding=2)
-        self.pool_2 = torch.nn.MaxPool2d(kernel_size=3, stride=2)
+        self.conv_2 = nn.Conv2d(5, 5, kernel_size=256, stride=1, padding=2)
+        self.pool_2 = nn.MaxPool2d(kernel_size=3, stride=2)
 
-        self.conv_3 = torch.nn.Conv2d(3, 3, kernel_size=384, stride=1, padding=1)
+        self.conv_3 = nn.Conv2d(3, 3, kernel_size=384, stride=1, padding=1)
 
-        self.conv_4 = torch.nn.Conv2d(3, 3, kernel_size=384, stride=1, padding=1)
+        self.conv_4 = nn.Conv2d(3, 3, kernel_size=384, stride=1, padding=1)
 
-        self.conv_5 = torch.nn.Conv2d(3, 3, kernel_size=256, stride=1, padding=1)
-        self.pool_5 = torch.nn.MaxPool2d(kernel_size=3, stride=2)
+        self.conv_5 = nn.Conv2d(3, 3, kernel_size=256, stride=1, padding=1)
+        self.pool_5 = nn.MaxPool2d(kernel_size=3, stride=2)
 
-        self.flat_1 = torch.nn.Linear(9216, 4096)
-        self.flat_2 = torch.nn.Linear(4096, 4096)
-        self.flat_2 = torch.nn.Linear(4096, 1000)
+        self.flat_1 = nn.Linear(9216, 4096)
+        self.flat_2 = nn.Linear(4096, 4096)
+        self.flat_3 = nn.Linear(4096, 1000)
+
+        self.dropout = nn.Dropout(0.5)
 
     def forward(self, x):
 
@@ -46,9 +54,43 @@ class AlexNet(torch.nn.module):
 
         x = x.view(-1, 9216)
 
-        x = F.relu(self.flat_1(x))
-        x = F.relu(self.flat_2(x))
+        x = self.dropout(F.relu(self.flat_1(x)))
+        x = self.dropout(F.relu(self.flat_2(x)))
 
         x = F.softmax(self.flat_3(x))
 
         return x
+
+
+if __name__ == '__main__':
+    an = AlexNet()
+
+    import torch.optim as optim
+
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.SGD(an.parameters(), lr=0.001, momentum=0.9)
+
+    for epoch in range(2):  # loop over the dataset multiple times
+
+        running_loss = 0.0
+        for i, data in enumerate(trainloader, 0):
+            # get the inputs
+            inputs, labels = data
+
+            # zero the parameter gradients
+            optimizer.zero_grad()
+
+            # forward + backward + optimize
+            outputs = an(inputs)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
+
+            # print statistics
+            running_loss += loss.item()
+            if i % 2000 == 1999:  # print every 2000 mini-batches
+                print('[%d, %5d] loss: %.3f' %
+                      (epoch + 1, i + 1, running_loss / 2000))
+                running_loss = 0.0
+
+    print('Finished Training')
